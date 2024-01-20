f83db45db4b8824671bbae5ad5b8925f381fc30c
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-01-20 12:04:27 +0100

    kogata: add teams

--- hosts/kogata/default.nix
+++ hosts/kogata/default.nix
@@ -1,10 +1,15 @@
 {
-  configurations.darwin.kogata = {pkgs, ...}: {
+  configurations.darwin.kogata = {pkgs, lib, ...}: {
     nixpkgs.system = "aarch64-darwin";
 
     settei.user.config = {
       home.packages = with pkgs; [alacritty];
     };
+
+    # TODO: Make it a settei module so it's easy to concatenate which pkgs are allowed
+    nixpkgs.config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [ "teams" ];
+    environment.systemPackages = with pkgs; [teams];
 
     common.hercules.enable = true;
   };
