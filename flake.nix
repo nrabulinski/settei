e8161c46acefee3a3b56e0fcc3674eb8f408d39b
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-02-01 20:59:50 +0100

    flake.nix: use extra- keys in nixConfig

--- flake.nix
+++ flake.nix
@@ -114,7 +114,7 @@
       "https://hercules-ci.cachix.org"
       "https://nrabulinski.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
