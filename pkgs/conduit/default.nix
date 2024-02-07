7fd058598ef41ca9a868103644fc81086822eea3
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-02-07 12:36:22 +0100

    pkgs/conduit: add required apple framework

--- pkgs/conduit/default.nix
+++ pkgs/conduit/default.nix
@@ -22,7 +22,11 @@ in
 
     nativeBuildInputs = [rustPlatform.bindgenHook];
 
-    buildInputs = lib.optionals stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
+    buildInputs = lib.optionals stdenv.isDarwin [
+      libiconv
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
 
     # Use system RocksDB
     ROCKSDB_INCLUDE_DIR = "${rocksdb}/include";
