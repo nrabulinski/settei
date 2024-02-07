87042ee8ea82a21cfe220509cea21e055da0509f
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-02-07 14:15:01 +0100

    hosts/kazuki/storage: disable timeout

--- hosts/kazuki/storage.nix
+++ hosts/kazuki/storage.nix
@@ -15,7 +15,6 @@
       "iocharset=utf8"
       "x-systemd.automount"
       "noauto"
-      "x-systemd.idle-timeout=60"
       "x-systemd.device-timeout=5s"
       "x-systemd.mount-timeout=5s"
       "uid=${toString config.users.users.atticd.uid}"
