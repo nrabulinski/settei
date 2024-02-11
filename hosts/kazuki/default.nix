b88c92756335f3575cad3b5edcc1bb9c40d6c560
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-02-11 15:45:01 +0100

    hosts/kazuki: ipv6

--- hosts/kazuki/default.nix
+++ hosts/kazuki/default.nix
@@ -25,6 +25,14 @@
       loader.systemd-boot.configurationLimit = 1;
       loader.efi.canTouchEfiVariables = true;
     };
+    systemd.network.enable = true;
+    systemd.network.networks."10-wan" = {
+      matchConfig.Name = "enp1s0";
+      networkConfig.DHCP = "ipv4";
+      address = ["2a01:4f8:c012:e5c::/64"];
+      routes = [{routeConfig.Gateway = "fe80::1";}];
+    };
+    networking.useNetworkd = true;
 
     common.hercules.enable = true;
     common.github-runner = {
