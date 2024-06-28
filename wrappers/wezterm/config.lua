5466c090ff1853fe19a71cb147b3ecf67b021d67
Nikodem Rabuliński <nrabulinski@antmicro.com>
2024-06-28 10:29:28 +0200

    wrappers/wezterm: configure for linux

--- wrappers/wezterm/config.lua
+++ wrappers/wezterm/config.lua
@@ -1,10 +1,18 @@
 local wezterm = require 'wezterm'
 
-return {
+local cfg = {
   color_scheme = 'Default Dark (base16)',
   enable_tab_bar = false,
-  window_decorations = 'RESIZE',
   font = wezterm.font('Iosevka Nerd Font'),
-  font_size = 14.0,
+  window_decorations = 'TITLE | RESIZE',
+  font_size = 10.5,
   native_macos_fullscreen_mode = true,
+  hide_mouse_cursor_when_typing = false,
 }
+
+if string.find(wezterm.target_triple, "darwin") then
+  cfg.font_size = 14.0
+  cfg.window_decorations = 'RESIZE'
+end
+
+return cfg
