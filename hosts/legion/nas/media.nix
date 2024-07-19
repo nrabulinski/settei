a1d60a61074245c48a5efb09c64b08a94ca24a40
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-07-19 22:31:18 +0200

    hosts/legion/nas: enable restic, make services depend on zfs mount

--- hosts/legion/nas/media.nix
+++ hosts/legion/nas/media.nix
@@ -1,4 +1,4 @@
-{ username, ... }:
+{ username, lib, ... }:
 {
   services.jellyfin.enable = true;
   services.radarr.enable = true;
@@ -10,6 +10,11 @@
     config.download_location = "/media/deluge";
   };
 
+  services.restic.server = {
+    enable = true;
+    dataDir = "/media/restic";
+  };
+
   users.users = {
     jellyfin.extraGroups = [
       "radarr"
@@ -19,4 +24,19 @@
     sonarr.extraGroups = [ "deluge" ];
     ${username}.extraGroups = [ "deluge" ];
   };
+
+  systemd.services =
+    lib.genAttrs
+      [
+        "jellyfin"
+        "radarr"
+        "sonarr"
+        "prowlarr"
+        "deluged"
+        "restic-rest-server"
+      ]
+      (_: {
+        requires = [ "zfs-mount.service" ];
+        after = [ "zfs-mount.service" ];
+      });
 }
