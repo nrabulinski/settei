69ea07184f00b707cd9154eb7cd2a84226f864e0
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-09-14 18:38:27 +0200

    wrappers/fish: add yazi

--- wrappers/fish/config.fish
+++ wrappers/fish/config.fish
@@ -25,4 +25,12 @@ and begin
     # Integrations
     zoxide init fish | source
     direnv hook fish | source
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            __zoxide_cd_internal -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
 end
