21e423b811e681b9d0b0bcc73c3517667b256ad2
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-11-01 13:19:37 +0100

    wrappers/wezterm: map C-CR and S-CR

--- wrappers/wezterm/config.lua
+++ wrappers/wezterm/config.lua
@@ -9,6 +9,10 @@ local cfg = {
   native_macos_fullscreen_mode = true,
   hide_mouse_cursor_when_typing = false,
   front_end = "WebGpu",
+  keys = {
+    { key = "Enter", mods = "CTRL",  action = wezterm.action { SendString = "\x1b[13;5u" } },
+    { key = "Enter", mods = "SHIFT", action = wezterm.action { SendString = "\x1b[13;2u" } },
+  },
 }
 
 if string.find(wezterm.target_triple, "darwin") then
