d56d339de779cf9933e8804ded717ac5b3175d40
Nikodem Rabuliński <nrabulinski@antmicro.com>
2024-11-01 13:07:01 +0100

    pkgs: git-fixup

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, inputs', ... }:
+    { pkgs, lib, inputs', ... }:
     {
       packages.conduit-next = pkgs.callPackage ./conduit {
         src = inputs.conduit-src;
@@ -14,6 +14,16 @@
         text = ''
           GITDIR="$(git rev-parse --git-dir)"
           git commit -eF "$GITDIR/COMMIT_EDITMSG"
+        '';
+      };
+
+      packages.git-fixup = pkgs.writeShellApplication {
+        name = "git-fixup";
+        text = ''
+          git log -n 50 --pretty=format:'%h %s' --no-merges | \
+          ${lib.getExe pkgs.fzf} | \
+          cut -c -7 | \
+          xargs -o git commit --fixup
         '';
       };
     };
