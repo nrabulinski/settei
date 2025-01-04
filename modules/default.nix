52c84814e87bd89841d36d50bec4f12b5b286f4f
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-01-04 19:56:10 +0100

    modules/flake: flatten

--- modules/default.nix
+++ modules/default.nix
@@ -1,11 +1,23 @@
 {
+  inputs,
+  ...
+}:
+let
+  flakeModule = import ./flake { inherit (inputs) nixpkgs darwin home-manager; };
+in
+{
   imports = [
     ./system
-    ./flake
+    flakeModule
   ];
 
   flake.homeModules = rec {
     settei = ./home;
+    default = settei;
+  };
+
+  flake.flakeModules = rec {
+    settei = flakeModule;
     default = settei;
   };
 }
