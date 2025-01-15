801b72b4eeb13500a8c73cd0fbccb464966bafba
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-01-15 20:41:55 +0100

    wrappers/wezterm: WebGpu frontend no longer needed

--- wrappers/wezterm/config.lua
+++ wrappers/wezterm/config.lua
@@ -8,7 +8,6 @@ local cfg = {
   font_size = 10.5,
   native_macos_fullscreen_mode = true,
   hide_mouse_cursor_when_typing = false,
-  front_end = "WebGpu",
   keys = {
     { key = "Enter", mods = "CTRL",  action = wezterm.action { SendString = "\x1b[13;5u" } },
     { key = "Enter", mods = "SHIFT", action = wezterm.action { SendString = "\x1b[13;2u" } },
