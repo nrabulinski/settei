dcb2f78a9c0aabbacd296340f868b906b5fae9a3
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-01-17 10:56:13 +0100

    hosts/youko: add kernel module for fan control

--- hosts/youko/hardware.nix
+++ hosts/youko/hardware.nix
@@ -1,5 +1,7 @@
+{ config, ... }:
 {
   boot = {
+    extraModulePackages = with config.boot.kernelPackages; [ it87 ];
     initrd.availableKernelModules = [
       "nvme"
       "xhci_pci"
@@ -11,7 +13,11 @@
     kernelModules = [
       "kvm-amd"
       "i2c-dev"
+      "it87"
     ];
+    extraModprobeConfig = ''
+      options it87 ignore_resource_conflict=1
+    '';
   };
 
   services.smartd.enable = true;
