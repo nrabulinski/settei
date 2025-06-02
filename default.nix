a6f43b43b2d216186f209642db153a6bc8dcbfb6
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-06-02 15:03:12 +0200

    default.nix: reexport system configurations

--- default.nix
+++ default.nix
@@ -5,4 +5,8 @@ in
 {
   ci.check = getPackage "ci-check";
   formatter = getPackage "formatter";
+  systems = {
+    nixos = builtins.mapAttrs (_: system: system.result) nilla.systems.nixos;
+    darwin = builtins.mapAttrs (_: system: system.result) nilla.systems.darwin;
+  };
 }
