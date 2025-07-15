863df540235fd56dd945eab38d4e7cbe9d627b71
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-07-15 21:44:12 +0200

    wrappers/fish: universal abbrs don't exist anymore, --global is redundant

--- wrappers/fish/config.fish
+++ wrappers/fish/config.fish
@@ -4,26 +4,26 @@ and begin
 
     # Abbreviations
     ## nix
-    abbr --add --global -- ns 'nix shell'
-    abbr --add --global -- nss 'nix search'
-    abbr --add --global -- nfu 'nix flake update'
+    abbr --add -- ns 'nix shell'
+    abbr --add -- nss 'nix search'
+    abbr --add -- nfu 'nix flake update'
     ## git
-    abbr --add --global -- gs 'git status'
-    abbr --add --global -- gp 'git pull'
-    abbr --add --global -- gps 'git push'
-    abbr --add --global -- gc 'git commit'
-    abbr --add --global -- gca 'git commit --amend --no-edit'
-    abbr --add --global -- gch 'git checkout'
-    abbr --add --global -- gss 'git switch'
-    abbr --add --global -- ga 'git add'
-    abbr --add --global -- gr 'git rebase'
-    abbr --add --global -- gri 'git rebase -i --autosquash'
-    abbr --add --global -- grc 'git rebase --continue'
-    abbr --add --global -- gra 'git rebase --abort'
-    abbr --add --global -- gd 'git diff'
-    abbr --add --global -- gdd 'git diff --cached'
-    abbr --add --global -- gl 'git log'
-    abbr --add --global -- gf 'git fixup' # See pkgs/default.nix
+    abbr --add -- gs 'git status'
+    abbr --add -- gp 'git pull'
+    abbr --add -- gps 'git push'
+    abbr --add -- gc 'git commit'
+    abbr --add -- gca 'git commit --amend --no-edit'
+    abbr --add -- gch 'git checkout'
+    abbr --add -- gss 'git switch'
+    abbr --add -- ga 'git add'
+    abbr --add -- gr 'git rebase'
+    abbr --add -- gri 'git rebase -i --autosquash'
+    abbr --add -- grc 'git rebase --continue'
+    abbr --add -- gra 'git rebase --abort'
+    abbr --add -- gd 'git diff'
+    abbr --add -- gdd 'git diff --cached'
+    abbr --add -- gl 'git log'
+    abbr --add -- gf 'git fixup' # See pkgs/default.nix
 
     # Aliases
     alias cat bat
