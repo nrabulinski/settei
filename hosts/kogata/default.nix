60af3b9bc54fbe91ef0a4076d84281cba4a3ecfb
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-11-17 00:40:01 +0100

    hosts/kogata: no more teams

--- hosts/kogata/default.nix
+++ hosts/kogata/default.nix
@@ -1,19 +1,12 @@
 {
-  config.systems.darwin.kogata.module =
-    { pkgs, ... }:
-    {
-      nixpkgs.hostPlatform = "aarch64-darwin";
+  config.systems.darwin.kogata.module = {
+    nixpkgs.hostPlatform = "aarch64-darwin";
 
-      settei.user.config.settei.desktop.enable = true;
+    settei.user.config.settei.desktop.enable = true;
 
-      settei.unfree.allowedPackages = [ "teams" ];
-      environment.systemPackages = with pkgs; [
-        (teams.overrideAttrs { sourceRoot = "Microsoft Teams.app"; })
-      ];
-
-      settei.tailscale = {
-        ipv4 = "100.102.13.61";
-        ipv6 = "fd7a:115c:a1e0::e126:d3d";
-      };
+    settei.tailscale = {
+      ipv4 = "100.102.13.61";
+      ipv6 = "fd7a:115c:a1e0::e126:d3d";
     };
+  };
 }
