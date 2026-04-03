811a876807fc22ca2a931593cf04fefb01833a5d
Nikodem Rabuliński <nikodem@rabulinski.com>
2026-04-03 12:47:22 +0200

    services/matrix: continuwuity from main

--- services/matrix/continuwuity.nix
+++ services/matrix/continuwuity.nix
@@ -1,12 +1,39 @@
 { config, pkgs, ... }:
+let
+  r = pkgs.fetchFromGitea {
+    domain = "forgejo.ellis.link";
+    owner = "continuwuation";
+    repo = "rocksdb";
+    rev = "10.10.fb";
+    hash = "sha256-1ef75IDMs5Hba4VWEyXPJb02JyShy5k4gJfzGDhopRk=";
+  };
+
+  c = pkgs.fetchFromGitea {
+    domain = "forgejo.ellis.link";
+    owner = "continuwuation";
+    repo = "continuwuity";
+    rev = "aa7907241194fa90f0e316a24137eb8d4d91b15e";
+    hash = "sha256-oHqWrqA8hyqxLoIAOw2nGXNd/9NfoQoOapZ7hppBV7U=";
+  };
+
+  fetchFromGitea = a: if a.repo == "continuwuity" then c else r;
+
+  rustPlatform = pkgs.rustPlatform // {
+    buildRustPackage =
+      argsFn:
+      pkgs.rustPlatform.buildRustPackage (
+        (argsFn { })
+        // {
+          cargoHash = "sha256-9qnPIbSPLSz+ZJZZjJBURTp5nrgngK8rufdM65ZtiAM=";
+        }
+      );
+  };
+in
 {
   services.matrix-continuwuity = {
     enable = true;
     package = pkgs.matrix-continuwuity.override {
-      rustPlatform = pkgs.makeRustPlatform {
-        inherit (pkgs) rustc cargo;
-        stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.stdenv;
-      };
+      inherit rustPlatform fetchFromGitea;
     };
     settings.global = {
       address = [ "127.0.0.1" ];
